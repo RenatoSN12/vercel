a295aa307720ed524b20ae35d732bf9f8fbcb97a
RenatoSN12 <rsnascimento159@gmail.com>
2025-01-29 21:16:57 -0300

    Commit

---
+++ components/NotionAdvantages.js
@@ -0,0 +1,50 @@
+export default function NotionAdvantage() {
+    return(
+    <section className="px-10">
+        <section className='flex items-center justify-around'>
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold mb-4">Organização Simplificada</h1>
+          <p>
+            Tenha todas as suas finanças em um único lugar, facilitando o acompanhamento de gastos, receitas e investimentos de forma clara e organizada.
+          </p>
+        </div>
+        <div className="w-1/2 flex justify-center">
+          <img
+            src="/second-image-main.jpg"
+            alt="Imagem Ilustrativa"
+            className="max-w-[700px] h-auto rounded-2xl"
+          />
+        </div>
+    </section>
+    <section className='flex items-center justify-around'>
+        <div className="w-1/2 flex justify-center">
+          <img
+            src="/placeholder.png"
+            alt="Imagem Ilustrativa"
+            className="max-w-[500px] h-auto"
+          />
+        </div>
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold mb-4">Decisões Financeiras Mais Inteligentes</h1>
+          <p>
+            Acesse relatórios e gráficos detalhados que ajudam a entender seus hábitos de gastos e planejar um futuro financeiro mais seguro.
+          </p>
+        </div>
+    </section>
+    <section className='flex items-center justify-around'>
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold mb-4">Acesso Conveniente em Qualquer Lugar</h1>
+          <p>
+            Gerencie suas finanças de qualquer dispositivo, a qualquer momento, com sincronização em tempo real e suporte offline quando necessário.
+          </p>
+        </div>
+        <div className="w-1/2 flex justify-center">
+          <img
+            src="/placeholder.png"
+            alt="Imagem Ilustrativa"
+            className="max-w-[500px] h-auto"
+          />
+        </div>
+    </section>
+    </section>   
+)};
